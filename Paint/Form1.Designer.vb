606f47f8692fa6fe0b88aabe72a7a44638862c33
JoshuaMiguel21 <joshuamiguel.embestro.cics@ust.edu.ph>
2024-05-11 17:21:57 +0800

    Add project files.

---
+++ Paint/Form1.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        ColorBlue = New PictureBox()
+        ColorRed = New PictureBox()
+        ColorGreen = New PictureBox()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Label1 = New Label()
+        ColorOrange = New PictureBox()
+        ColorYellow = New PictureBox()
+        ColorPink = New PictureBox()
+        ColorBlack = New PictureBox()
+        Eraser = New PictureBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        CType(ColorBlue, ComponentModel.ISupportInitialize).BeginInit()
+        CType(ColorRed, ComponentModel.ISupportInitialize).BeginInit()
+        CType(ColorGreen, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(ColorOrange, ComponentModel.ISupportInitialize).BeginInit()
+        CType(ColorYellow, ComponentModel.ISupportInitialize).BeginInit()
+        CType(ColorPink, ComponentModel.ISupportInitialize).BeginInit()
+        CType(ColorBlack, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Eraser, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' ColorBlue
+        ' 
+        ColorBlue.BackgroundImageLayout = ImageLayout.Stretch
+        ColorBlue.Cursor = Cursors.Hand
+        ColorBlue.Image = CType(resources.GetObject("ColorBlue.Image"), Image)
+        ColorBlue.Location = New Point(314, 28)
+        ColorBlue.Margin = New Padding(3, 2, 3, 2)
+        ColorBlue.Name = "ColorBlue"
+        ColorBlue.Size = New Size(88, 70)
+        ColorBlue.SizeMode = PictureBoxSizeMode.StretchImage
+        ColorBlue.TabIndex = 1
+        ColorBlue.TabStop = False
+        ' 
+        ' ColorRed
+        ' 
+        ColorRed.BackgroundImageLayout = ImageLayout.Stretch
+        ColorRed.Cursor = Cursors.Hand
+        ColorRed.Image = CType(resources.GetObject("ColorRed.Image"), Image)
+        ColorRed.Location = New Point(217, 28)
+        ColorRed.Margin = New Padding(3, 2, 3, 2)
+        ColorRed.Name = "ColorRed"
+        ColorRed.Size = New Size(88, 70)
+        ColorRed.SizeMode = PictureBoxSizeMode.StretchImage
+        ColorRed.TabIndex = 2
+        ColorRed.TabStop = False
+        ' 
+        ' ColorGreen
+        ' 
+        ColorGreen.BackgroundImageLayout = ImageLayout.Stretch
+        ColorGreen.Cursor = Cursors.Hand
+        ColorGreen.Image = CType(resources.GetObject("ColorGreen.Image"), Image)
+        ColorGreen.Location = New Point(412, 28)
+        ColorGreen.Margin = New Padding(3, 2, 3, 2)
+        ColorGreen.Name = "ColorGreen"
+        ColorGreen.Size = New Size(87, 71)
+        ColorGreen.SizeMode = PictureBoxSizeMode.StretchImage
+        ColorGreen.TabIndex = 3
+        ColorGreen.TabStop = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        PictureBox1.BorderStyle = BorderStyle.FixedSingle
+        PictureBox1.Location = New Point(56, 124)
+        PictureBox1.Margin = New Padding(3, 2, 3, 2)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(920, 397)
+        PictureBox1.TabIndex = 4
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(-66, -35)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(359, 198)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 5
+        PictureBox2.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Juice ITC", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.ForeColor = Color.LavenderBlush
+        Label1.Location = New Point(4, 529)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(139, 21)
+        Label1.TabIndex = 6
+        Label1.Text = "V1.1 par le Groupe 1"
+        ' 
+        ' ColorOrange
+        ' 
+        ColorOrange.BackgroundImageLayout = ImageLayout.Stretch
+        ColorOrange.Cursor = Cursors.Hand
+        ColorOrange.Image = CType(resources.GetObject("ColorOrange.Image"), Image)
+        ColorOrange.Location = New Point(509, 28)
+        ColorOrange.Margin = New Padding(3, 2, 3, 2)
+        ColorOrange.Name = "ColorOrange"
+        ColorOrange.Size = New Size(87, 71)
+        ColorOrange.SizeMode = PictureBoxSizeMode.StretchImage
+        ColorOrange.TabIndex = 7
+        ColorOrange.TabStop = False
+        ' 
+        ' ColorYellow
+        ' 
+        ColorYellow.BackgroundImageLayout = ImageLayout.Stretch
+        ColorYellow.Cursor = Cursors.Hand
+        ColorYellow.Image = CType(resources.GetObject("ColorYellow.Image"), Image)
+        ColorYellow.Location = New Point(606, 27)
+        ColorYellow.Margin = New Padding(3, 2, 3, 2)
+        ColorYellow.Name = "ColorYellow"
+        ColorYellow.Size = New Size(87, 71)
+        ColorYellow.SizeMode = PictureBoxSizeMode.StretchImage
+        ColorYellow.TabIndex = 8
+        ColorYellow.TabStop = False
+        ' 
+        ' ColorPink
+        ' 
+        ColorPink.BackgroundImageLayout = ImageLayout.Stretch
+        ColorPink.Cursor = Cursors.Hand
+        ColorPink.Image = CType(resources.GetObject("ColorPink.Image"), Image)
+        ColorPink.Location = New Point(704, 28)
+        ColorPink.Margin = New Padding(3, 2, 3, 2)
+        ColorPink.Name = "ColorPink"
+        ColorPink.Size = New Size(87, 71)
+        ColorPink.SizeMode = PictureBoxSizeMode.StretchImage
+        ColorPink.TabIndex = 9
+        ColorPink.TabStop = False
+        ' 
+        ' ColorBlack
+        ' 
+        ColorBlack.BackgroundImageLayout = ImageLayout.Stretch
+        ColorBlack.Cursor = Cursors.Hand
+        ColorBlack.Image = CType(resources.GetObject("ColorBlack.Image"), Image)
+        ColorBlack.Location = New Point(802, 28)
+        ColorBlack.Margin = New Padding(3, 2, 3, 2)
+        ColorBlack.Name = "ColorBlack"
+        ColorBlack.Size = New Size(87, 71)
+        ColorBlack.SizeMode = PictureBoxSizeMode.StretchImage
+        ColorBlack.TabIndex = 10
+        ColorBlack.TabStop = False
+        ' 
+        ' Eraser
+        ' 
+        Eraser.BackColor = Color.Transparent
+        Eraser.Cursor = Cursors.Hand
+        Eraser.Image = CType(resources.GetObject("Eraser.Image"), Image)
+        Eraser.Location = New Point(858, -1)
+        Eraser.Name = "Eraser"
+        Eraser.Size = New Size(186, 138)
+        Eraser.SizeMode = PictureBoxSizeMode.Zoom
+        Eraser.TabIndex = 11
+        Eraser.TabStop = False
+        ' 
+        ' Label2
+        ' 
+        Label2.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Juice ITC", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.ForeColor = Color.LavenderBlush
+        Label2.Location = New Point(440, 4)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(174, 21)
+        Label2.TabIndex = 12
+        Label2.Text = "Choisissez votre couleur."
+        ' 
+        ' Label3
+        ' 
+        Label3.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Juice ITC", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.ForeColor = Color.LavenderBlush
+        Label3.Location = New Point(440, 100)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(173, 21)
+        Label3.TabIndex = 13
+        Label3.Text = "Currently using: ""Color"""
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSize = True
+        AutoSizeMode = AutoSizeMode.GrowAndShrink
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(1031, 551)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(ColorBlack)
+        Controls.Add(ColorPink)
+        Controls.Add(ColorYellow)
+        Controls.Add(ColorOrange)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Controls.Add(ColorGreen)
+        Controls.Add(ColorRed)
+        Controls.Add(ColorBlue)
+        Controls.Add(PictureBox2)
+        Controls.Add(Eraser)
+        DoubleBuffered = True
+        Margin = New Padding(3, 2, 3, 2)
+        MaximizeBox = False
+        MdiChildrenMinimizedAnchorBottom = False
+        Name = "Form1"
+        Text = "Oui Oui We Paint"
+        CType(ColorBlue, ComponentModel.ISupportInitialize).EndInit()
+        CType(ColorRed, ComponentModel.ISupportInitialize).EndInit()
+        CType(ColorGreen, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(ColorOrange, ComponentModel.ISupportInitialize).EndInit()
+        CType(ColorYellow, ComponentModel.ISupportInitialize).EndInit()
+        CType(ColorPink, ComponentModel.ISupportInitialize).EndInit()
+        CType(ColorBlack, ComponentModel.ISupportInitialize).EndInit()
+        CType(Eraser, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents ColorBlue As PictureBox
+    Friend WithEvents ColorRed As PictureBox
+    Friend WithEvents ColorGreen As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ColorOrange As PictureBox
+    Friend WithEvents ColorYellow As PictureBox
+    Friend WithEvents ColorPink As PictureBox
+    Friend WithEvents ColorBlack As PictureBox
+    Friend WithEvents Eraser As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+
+End Class
---
+++ Paint/Form1.vb
@@ -0,0 +1,97 @@
+﻿Imports System.Drawing
+Imports System.Reflection.Emit
+
+Public Class Form1
+    Private drawing As Boolean = False
+    Private brushSize As Integer = 30
+    Private lastPoint As Point
+    Private currentColor As Color = Color.Black
+    Private pointList As New List(Of Tuple(Of Point, Point, Color))()
+
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        drawing = True
+        lastPoint = e.Location
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        If drawing Then
+            pointList.Add(Tuple.Create(lastPoint, e.Location, currentColor))
+            lastPoint = e.Location
+            PictureBox1.Invalidate()
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseUp
+        drawing = False
+    End Sub
+
+    Private Sub PictureBox1_Paint(sender As Object, e As PaintEventArgs) Handles PictureBox1.Paint
+        e.Graphics.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Using pen As New Pen(Color.Black, 10)
+            pen.LineJoin = Drawing2D.LineJoin.Round
+            pen.EndCap = Drawing2D.LineCap.Round
+            pen.StartCap = Drawing2D.LineCap.Round
+            For Each pointColor In pointList
+                pen.Color = pointColor.Item3
+                e.Graphics.DrawLine(pen, pointColor.Item1, pointColor.Item2)
+            Next
+        End Using
+    End Sub
+
+    Private Sub ColorBlue_Click(sender As Object, e As EventArgs) Handles ColorBlue.Click
+        Label3.Text = "Currently using: Blue"
+        currentColor = Color.Blue
+    End Sub
+
+    Private Sub ColorRed_Click(sender As Object, e As EventArgs) Handles ColorRed.Click
+        Label3.Text = "Currently using: Red"
+        currentColor = Color.Red
+    End Sub
+
+    Private Sub ColorGreen_Click(sender As Object, e As EventArgs) Handles ColorGreen.Click
+        Label3.Text = "Currently using: Green"
+        currentColor = Color.Green
+    End Sub
+
+    Private Sub ColorOrange_Click(sender As Object, e As EventArgs) Handles ColorOrange.Click
+        Label3.Text = "Currently using: Orange"
+        currentColor = Color.Orange
+    End Sub
+
+    Private Sub ColorYellow_Click(sender As Object, e As EventArgs) Handles ColorYellow.Click
+        Label3.Text = "Currently using: Yellow"
+        currentColor = Color.Yellow
+    End Sub
+
+    Private Sub ColorPink_Click(sender As Object, e As EventArgs) Handles ColorPink.Click
+        Label3.Text = "Currently using: Pink"
+        currentColor = Color.Pink
+    End Sub
+
+    Private Sub ColorBlack_Click(sender As Object, e As EventArgs) Handles ColorBlack.Click
+        Label3.Text = "Currently using: Black"
+        currentColor = Color.Black
+    End Sub
+
+    Private Sub PictureBox1_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox1.MouseEnter
+        Dim circleCursor As New Cursor(CreateCircleCursor(brushSize, currentColor))
+        PictureBox1.Cursor = circleCursor
+    End Sub
+
+    Private Sub Eraser_Click(sender As Object, e As EventArgs) Handles Eraser.Click
+        Label3.Text = "Currently using: Eraser"
+        currentColor = PictureBox1.BackColor
+    End Sub
+
+    Private Function CreateCircleCursor(size As Integer, color As Color) As IntPtr
+        Dim smallerSize As Integer = size - 10 ' Adjust the size here
+        Dim bm As New Bitmap(size, size)
+        Dim g As Graphics = Graphics.FromImage(bm)
+        Dim borderPen As New Pen(Color.Black, 2) ' Specify the border color and width
+        g.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+        g.FillEllipse(New SolidBrush(color), (size - smallerSize) \ 2, (size - smallerSize) \ 2, smallerSize, smallerSize)
+        g.DrawEllipse(borderPen, (size - smallerSize) \ 2, (size - smallerSize) \ 2, smallerSize, smallerSize) ' Draw the border
+        g.Dispose()
+        Return bm.GetHicon()
+    End Function
+End Class
